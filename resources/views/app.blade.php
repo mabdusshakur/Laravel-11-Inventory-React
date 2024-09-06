c5ec2821d9e933c87ca392d4b5e7e346ba47ec08
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-06 18:18:57 +0600

    root app template added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html data-bs-theme="light" lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
     @viteReactRefresh
     @vite('resources/js/app.jsx')
-
     @inertiaHead
 </head>
 
