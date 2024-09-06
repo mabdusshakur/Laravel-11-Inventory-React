cab76d8db84ec2afbdc4264a933ef1e44d9633da
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-07 00:08:29 +0600

    root jsx updated

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,9 @@
 import './bootstrap';
 import './config';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { Inertia } from "@inertiajs/inertia";
