2b2b3397cafe1ff519f4ea7ea4b7893bc21ac168
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-06 18:18:14 +0600

    verifyOtp page added

---
+++ resources/js/Pages/Auth/VerifyOtpPage.jsx
@@ -0,0 +1,10 @@
+import VerifyOtpForm from "../../Components/Auth/VerifyOtpForm";
+import AppLayout from "../../Layouts/AppLayout";
+
+export default function Login() {
+    return (
+        <AppLayout>
+            <VerifyOtpForm />
+        </AppLayout>
+    );
+}
