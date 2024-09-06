b05159eb6a341dd4a8258eef40bbc33621c58b5a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-06 18:01:48 +0600

    app.jsx added

---
+++ resources/js/app.jsx
@@ -0,0 +1,15 @@
+import './bootstrap';
+import './config';
+
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
+})
