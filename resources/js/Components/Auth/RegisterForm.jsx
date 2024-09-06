3b771e350b1e5baf59934355b793d289ef3475eb
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-06 18:17:40 +0600

    register page added

---
+++ resources/js/Components/Auth/RegisterForm.jsx
@@ -0,0 +1,87 @@
+function RegisterForm() {
+    async function onRegistration() {
+
+        let email = document.getElementById('email').value;
+        let firstName = document.getElementById('firstName').value;
+        let lastName = document.getElementById('lastName').value;
+        let mobile = document.getElementById('mobile').value;
+        let password = document.getElementById('password').value;
+
+        if (email.length === 0) {
+            errorToast('Email is required')
+        } else if (firstName.length === 0) {
+            errorToast('First Name is required')
+        } else if (lastName.length === 0) {
+            errorToast('Last Name is required')
+        } else if (mobile.length === 0) {
+            errorToast('Mobile is required')
+        } else if (password.length === 0) {
+            errorToast('Password is required')
+        } else {
+            showLoader();
+            let res = await axios.post("/api/auth/register", {
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                mobile: mobile,
+                password: password
+            })
+            hideLoader();
+            if (res.status === 201 && res.data['success'] === true) {
+                successToast(res.data['message']);
+                setTimeout(function () {
+                    Inertia.visit('/login-page');
+                }, 2000)
+            } else {
+                errorToast(res.data['message'])
+            }
+        }
+    }
+
+    return (<>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-10 col-lg-10 center-screen">
+                    <div className="card animated fadeIn w-100 p-3">
+                        <div className="card-body">
+                            <h4>Sign Up</h4>
+                            <hr />
+                            <div className="container-fluid m-0 p-0">
+                                <div className="row m-0 p-0">
+                                    <div className="col-md-4 p-2">
+                                        <label>Email Address</label>
+                                        <input className="form-control" id="email" type="email" placeholder="User Email" />
+                                    </div>
+                                    <div className="col-md-4 p-2">
+                                        <label>First Name</label>
+                                        <input className="form-control" id="firstName" type="text" placeholder="First Name" />
+                                    </div>
+                                    <div className="col-md-4 p-2">
+                                        <label>Last Name</label>
+                                        <input className="form-control" id="lastName" type="text" placeholder="Last Name" />
+                                    </div>
+                                    <div className="col-md-4 p-2">
+                                        <label>Mobile Number</label>
+                                        <input className="form-control" id="mobile" type="mobile" placeholder="Mobile" />
+                                    </div>
+                                    <div className="col-md-4 p-2">
+                                        <label>Password</label>
+                                        <input className="form-control" id="password" type="password" placeholder="User Password" />
+                                    </div>
+                                </div>
+                                <div className="row m-0 p-0">
+                                    <div className="col-md-4 p-2">
+                                        <button className="btn w-100 bg-gradient-primary mt-3" onClick={onRegistration}>Complete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>);
+}
+
+export default RegisterForm;
---
+++ resources/js/Pages/Auth/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../Components/Auth/RegisterForm";
+import AppLayout from "../../Layouts/AppLayout";
+
+export default function Login() {
+    return (
+        <AppLayout>
+            <RegisterForm />
+        </AppLayout>
+    );
+}
