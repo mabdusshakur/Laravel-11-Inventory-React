43f0edec51d28584377854e27bfb2f17f9997edd
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-06 18:17:50 +0600

    resetPassword page added

---
+++ resources/js/Pages/Auth/ResetPasswordPage.jsx
@@ -0,0 +1,10 @@
+import ResetPasswordForm from "../../Components/Auth/ResetPasswordForm";
+import AppLayout from "../../Layouts/AppLayout";
+
+export default function Login() {
+    return (
+        <AppLayout>
+            <ResetPasswordForm />
+        </AppLayout>
+    );
+}
