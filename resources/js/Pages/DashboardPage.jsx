88a099f3b99330e2b339bcec9783aba56c0d0d17
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-07 18:22:16 +0600

    dashboard added

---
+++ resources/js/Pages/DashboardPage.jsx
@@ -0,0 +1,10 @@
+import Summary from "../Components/dashboard/Summary";
+import SidenavLayout from "../Layouts/SidenavLayout";
+
+export default function DashboardPage() {
+    return (
+        <SidenavLayout>
+            <Summary />
+        </SidenavLayout>
+    );
+}
